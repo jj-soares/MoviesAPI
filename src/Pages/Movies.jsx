09577ead29456af0cdffcb7bb04959462e45f8ba
jj-soares <joao.soares.9686@gmail.com>
2023-01-26 14:12:21 -0300

    Criaçao tela de detalhes do movie

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -27,15 +27,57 @@ function Movies() {
     setMovie(data)
   }
 
+  const formatCurrency = (number) => {
+    return number.toLocaleString ("en-US", {
+      style: "currency",
+      currency: "USD",
+    })
+  }
+
   useEffect(() => {
     const movieURL = `${moviesURL}${id}?${apiKey}`
 
     getMovie(movieURL)
   }, [])
 
-  
-  return <div>
-    {movie && <>{movie.title}</>}
+
+  return <div className="movie-page">
+    {movie && (
+      <>
+
+        <MoviesCard movie={movie} showlink={false} />
+        <p className="tagline" >{movie.tagline}</p>
+
+        <div className="info">
+          <h3>
+            <BsWallet2 /> Orçamento do filme :
+          </h3>
+          <p>{formatCurrency(movie.budget)}</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsGraphUp /> Receita :
+          </h3>
+          <p>{formatCurrency(movie.revenue)}</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsHourglassSplit /> Duração do filme :
+          </h3>
+          <p>{movie.runtime}</p>
+        </div>
+        <div className="info description">
+          <h3>
+            <BsFillFileEarmarkTextFill /> Descrição :
+          </h3>
+          <p>{movie.overview}</p>
+        </div>
+
+
+
+
+
+      </>)}
   </div>
 
 
